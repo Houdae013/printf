0c4d92ea4ed6c518a8b38201975853336abaad06
Houdae013 <houdaelyoussoufi5@gmail.com>
2023-08-21 15:03:01 -0700

    printf

--- _binary.c
+++ _binary.c
@@ -8,23 +8,35 @@
 
 int _binary(unsigned int b)
 {
-	int n = 0;
+	int n = 0, i = 0;
+	int bi[64];
 
 	if (b == 0)
+	{
+		n++;
 		_putchar('0');
+	}
 	while (b > 0)
 	{
 		if (b % 2 == 0)
 		{
 			b = b / 2;
-			_putchar('0');
+			bi[i] = 0;
+			i++;
 		}
 		else
 		{
 			b = b / 2;
-			_putchar('1');
+			bi[i] = 1;
+			i++;
 		}
 		n++;
+
+	}
+	while (i > 0)
+	{
+		_putchar('0' + bi[i - 1]);
+		i--;
 	}
 	return (n);
 }
