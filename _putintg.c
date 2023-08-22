8919d91e898b0ab56ed42ada43ca1d90d11ef6fc
Houdae013 <houdaelyoussoufi5@gmail.com>
2023-08-22 13:54:16 -0700

    printf

--- _putintg.c
+++ _putintg.c
@@ -1,22 +1,22 @@
 #include "main.h"
 /**
  * _putintg- print intigers
- * @in: intiger
- * return n
+ * @d: intiger
+ * Return: n
  */
 
 int _putintg(int d)
 {
 
 	int n = 0, co = 0;
-	
+
 	if (d == 0)
-		return 1;
+		return (1);
 	while (d != 0)
 	{
 		d /= 10;
 		co++;
 		n++;
 	}
-	return n;
+	return (n);
 }
