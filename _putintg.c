bb3008f4c87806aa473eeae18a6b991357c7a349
Imranekad <pilotestar@gmail.com>
2023-08-22 15:32:42 -0700

    ee

--- _putintg.c
+++ _putintg.c
@@ -14,18 +14,16 @@ int _putintg(int d)
 	return (1);
 	while (d != 0)
 	{
-	co=d%10;
-	d=d/10;
-	s[n]='0'+co;
-	n++;
+		co=d%10;
+		d=d/10;
+		s[n]='0'+co;
+		n++;
 	}
 	m=n;
-		20	        
-			21	        while (m>0)
-			22	        {
-				23	                putchar(s[m-1]);
-				24	                m--;
-				25	                
-					26	        }
-		27	        return (n);
+	while (m>0)
+	{
+		_putchar(s[m-1]);
+		m--;
+	}
+	return (n);
 }
