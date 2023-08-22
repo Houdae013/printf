cf75c06f3737f5f960c5a01271961336f795415f
Imranekad <pilotestar@gmail.com>
2023-08-22 09:30:45 -0700

    ee

--- _putintg.c
+++ _putintg.c
@@ -1,2 +1,21 @@
 #include "main.h"
+/**
+ * _putintg- print intigers
+ * @in: intiger
+ * return n
+ */
 
+int _putintg(int *in)
+{
+	int n = 0, co = 0;
+	if (in == NULL)
+	if (in == 0)
+		return 1;
+	while (in != 0)
+	{ 
+		in /= 10;
+		co++;
+		n++
+	}
+	return n;
+}
